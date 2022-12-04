9ce430bc05e23a9741c23bbca65fc80ff7e3f68c
Albada16 <NamelessOAO@gmail.com>
2022-12-04 23:15:14 +0800

    Update users_feed.py

--- db/users_feed.py
+++ db/users_feed.py
@@ -1,27 +1,27 @@
 users = [
     {
         "username": "Irene",
-        "avatar":"123123",
+        "avatar":"https://images.pexels.com/photos/12942114/pexels-photo-12942114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "email":"I1231@gamil.com"
     },
     {
         "username": "Seulgi",
-        "avatar": "123123",
+        "avatar": "https://images.pexels.com/photos/11098737/pexels-photo-11098737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "email": "S1231@gamil.com"
     },
     {
         "username": "Wendy",
-        "avatar": "123123",
+        "avatar": "https://images.pexels.com/photos/11139223/pexels-photo-11139223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "email": "W1231@gamil.com"
     },
     {
         "username": "Joy",
-        "avatar": "123123",
+        "avatar": "https://images.pexels.com/photos/14040258/pexels-photo-14040258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "email": "J1231@gamil.com"
     },
     {
         "username": "Yeri",
-        "avatar": "123123",
+        "avatar": "https://images.pexels.com/photos/12495840/pexels-photo-12495840.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "email": "Y1231@gamil.com"
     },
 ]

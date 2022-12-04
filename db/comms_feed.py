7676f52450b37da4a10d64aded4763099756b4e7
Albada16 <NamelessOAO@gmail.com>
2022-12-04 23:43:04 +0800

    Update comms_feed.py

--- db/comms_feed.py
+++ db/comms_feed.py
@@ -8,5 +8,63 @@ comms = [
         "content": "balabalabala,balabala.",
         "article_id": 2,
         "user_id": 5
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 2,
+        "user_id": 1
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 3,
+        "user_id": 2
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 3,
+        "user_id": 4
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 3,
+        "user_id": 3
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 4,
+        "user_id": 1
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 4,
+        "user_id": 4
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 5,
+        "user_id": 1
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 5,
+        "user_id": 2
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 5,
+        "user_id": 3
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 5,
+        "user_id": 4
+    },
+    {
+        "content": "balabalabala,balabala.",
+        "article_id": 5,
+        "user_id": 5
     }
+
+
+
 ]
